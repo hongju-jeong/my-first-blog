dbe15997b777f91ed9793b9c36aedd6f3f94133a
muzy06038 <sub06038@naver.com>
2022-03-25 06:33:09 +0000

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  # 모든 접속 요청을 blog.urls로 전송해 추가 명령
 ]
